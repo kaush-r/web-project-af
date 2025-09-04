3348af8930b6ffae0ea24764eba928f5e234356d
ShimalRas <shimalrashmitha.dev@gmail.com>
2025-09-04 10:10:57 +0530

    Making the home page and event page

---
+++ Events.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="Css stuff/main.css">
+    <title>Events</title>
+</head>
+<body>
+    <div class="navbar">
+        <nav>
+            <ul>
+                <li><a href="homepage.php">Home</a></li>
+                <li><a href="Events.php">Events</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+    <br>
+    <h1 style="text-align: center; color: #16d640;">Events Page</h1>
+    <p style="text-align: center; color: white;">Discover upcoming events and activities happening around you.</p>
+    <br>
+    <div style="text-align: center;">
+        <a href="main.html"><button class="button-backtohome">Back to Home</button></a>
+    </div>
+</body>
+</html>
---
+++ homepage.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="Css stuff/main.css">
+    <title>Event watch</title>
+</head>
+<body>
+    <div class="navbar">
+        <nav>
+            <ul>
+                <li><a href="homepage.php">Home</a></li>
+                <li><a href="Events.php">Events</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+    <br>
+    <h1 style="text-align: center; color: #16d640;">Welcome to Event Watch</h1>
+    <p style="text-align: center; color: white;">Your go-to platform for discovering and tracking events around you.</p>
+    <br>
+    
+    <div style="text-align: center;">
+        <a href="Events.html"> 
+<button class="button-exploreevents">
+   <svg class="svgIcon" viewBox="0 0 512 512" height="1em" xmlns="http://www.w3.org/2000/svg"><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm50.7-186.9L162.4 380.6c-19.4 7.5-38.5-11.6-31-31l55.5-144.3c3.3-8.5 9.9-15.1 18.4-18.4l144.3-55.5c19.4-7.5 38.5 11.6 31 31L325.1 306.7c-3.2 8.5-9.9 15.1-18.4 18.4zM288 256a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"></path></svg>
+  Explore
+</button>
+</a>
+    </div>
+</body>
+</html>
